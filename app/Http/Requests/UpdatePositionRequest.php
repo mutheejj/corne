163b39f5ed2625912d2f851d1842149b90beb7b6
mutheejj <johnmuthee547@gmail.com>
2026-07-15 16:22:27 +0300

    UpdatePositionRequest

---
+++ app/Http/Requests/UpdatePositionRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePositionRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->is_admin;
+    }
+
+    /**
+     * @return array<string, array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['sometimes', 'required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'max_votes' => ['sometimes', 'required', 'integer', 'min:1'],
+            'sort_order' => ['nullable', 'integer', 'min:0'],
+        ];
+    }
+}
