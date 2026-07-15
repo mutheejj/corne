38e3343eee3ef13e83f286f69b861b5764ebcb68
mutheejj <johnmuthee547@gmail.com>
2026-07-15 16:22:27 +0300

    UpdateProfileRequest

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'phone' => ['nullable', 'string', 'max:20'],
+            'avatar' => ['nullable', 'image', 'max:2048'],
+            'password' => ['nullable', 'confirmed', 'min:8'],
+        ];
+    }
+}
