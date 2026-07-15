7cc8af2fed7a3b5973cd89a19cbcaa79924215c1
mutheejj <johnmuthee547@gmail.com>
2026-07-15 16:22:27 +0300

    DisqualifyCandidateRequest

---
+++ app/Http/Requests/DisqualifyCandidateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DisqualifyCandidateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->is_admin;
+    }
+
+    /**
+     * @return array<string, array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'reason' => ['required', 'string', 'max:1000'],
+        ];
+    }
+}
