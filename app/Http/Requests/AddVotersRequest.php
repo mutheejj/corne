f2c7ca9f5e36916c5fb264c54a51698180e555e5
mutheejj <johnmuthee547@gmail.com>
2026-07-15 16:22:27 +0300

    AddVotersRequest

---
+++ app/Http/Requests/AddVotersRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddVotersRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->is_admin;
+    }
+
+    /**
+     * @return array<string, array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'voter_ids' => ['required', 'array'],
+            'voter_ids.*' => ['exists:users,id'],
+        ];
+    }
+}
