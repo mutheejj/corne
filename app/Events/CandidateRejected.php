c2e007e583dd748a91072db649e9f2ad558ad122
mutheejj <johnmuthee547@gmail.com>
2026-07-15 16:22:25 +0300

    CandidateRejected

---
+++ app/Events/CandidateRejected.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Candidate;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CandidateRejected
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public Candidate $candidate,
+        public ?string $reason = null,
+    ) {}
+
+    /**
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('candidate.'.$this->candidate->id),
+        ];
+    }
+}
